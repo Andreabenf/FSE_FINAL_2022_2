cbc16d442bece32c69d7adb36dfde389dbf66caa
André <andreabenf@gmail.com>
2023-02-13 23:56:36 -0300

    diferencia placas

--- client/main/mqtt.c
+++ client/main/mqtt.c
@@ -25,66 +25,81 @@ esp_mqtt_client_handle_t client;
 extern int flag_run;
 extern char central_path[100];
 
-static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event) {
+static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+{
     esp_mqtt_client_handle_t client = event->client;
 
-    switch (event->event_id) {
-        case MQTT_EVENT_CONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            if (flag_run == 0) {
-                esp_mqtt_client_publish(client, central_path,
-                                        define_modo_json(), 0, 1, 0);
-            }
-            // esp_mqtt_client_subscribe(client, central_path, 0);
-            esp_mqtt_client_subscribe(client, "v1/devices/me/rpc/request/+", 0);
+    switch (event->event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        if (flag_run == 0)
+        {
+            esp_mqtt_client_publish(client, central_path,
+                                    define_modo_json(), 0, 1, 0);
+        }
+        // esp_mqtt_client_subscribe(client, central_path, 0);
+        esp_mqtt_client_subscribe(client, "v1/devices/me/rpc/request/+", 0);
 
-            break;
-        case MQTT_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-            break;
-        case MQTT_EVENT_SUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-            break;
-        case MQTT_EVENT_UNSUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-            break;
-        case MQTT_EVENT_PUBLISHED:
-            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d, %s", event->msg_id, central_path);
-            break;
-        case MQTT_EVENT_DATA:
-            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-            const char *res = event->data;
-            parse_json(res);
-            break;
-        case MQTT_EVENT_ERROR:
-            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-            break;
-        default:
-            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-            break;
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d, %s", event->msg_id, central_path);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        const char *res = event->data;
+        parse_json(res);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
     }
     return ESP_OK;
 }
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
-                               int32_t event_id, void *event_data) {
+                               int32_t event_id, void *event_data)
+{
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base,
              event_id);
     mqtt_event_handler_cb(event_data);
 }
 
-void mqtt_start() {
+void mqtt_start()
+{
+#ifdef CONFIG_PLACA_1
+   esp_mqtt_client_config_t mqtt_config = {
+        .uri = "mqtt://164.41.98.25",
+        .username = "cUNn5ysSxbGiO0vIaiwQ"};
+#elif CONFIG_PLACA_2
     esp_mqtt_client_config_t mqtt_config = {
         .uri = "mqtt://164.41.98.25",
-        .username = "cUNn5ysSxbGiO0vIaiwQ"
-    };
+        .username = "XDJBSU5cJK2veAQrUc5y"};
+#elif CONFIG_PLACA_3
+    esp_mqtt_client_config_t mqtt_config = {
+        .uri = "mqtt://164.41.98.25",
+        .username = "XDJBSU5cJK2veAQrUc5y"};
+#endif
     client = esp_mqtt_client_init(&mqtt_config);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler,
                                    client);
     esp_mqtt_client_start(client);
 }
 
-void mqtt_envia_mensagem(char *topico, char *mensagem) {
+void mqtt_envia_mensagem(char *topico, char *mensagem)
+{
     int message_id = esp_mqtt_client_publish(client, topico, mensagem, 0, 1, 0);
     ESP_LOGI(TAG, "Mensagem enviada, ID: %d, %s", message_id, mensagem);
 }
